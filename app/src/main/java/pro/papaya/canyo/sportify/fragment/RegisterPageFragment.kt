e50ef528456220225842cd8b7a7fff1707b4751f
TodayWeSleep <canyousaaay@gmail.com>
2018-09-05 22:52:47 +0300

    Create view holders for register page

--- app/src/main/java/pro/papaya/canyo/sportify/fragment/RegisterPageFragment.kt
+++ app/src/main/java/pro/papaya/canyo/sportify/fragment/RegisterPageFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import pro.papaya.canyo.myapplication.R
+import java.util.*
 
 // Instances of this class are fragments representing a single
 // object in our collection.
@@ -25,6 +27,10 @@ class RegisterPageFragment : Fragment() {
 
     private var mCallback: Callback? = null
 
+    private lateinit var bioPageViewHolder: BioPageViewHolder
+    private lateinit var placePageViewHolder: PlacePageViewHolder
+    private lateinit var credentialsPageViewHolder: CredentialsPageViewHolder
+
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -36,6 +42,8 @@ class RegisterPageFragment : Fragment() {
 
         val rootView: View = inflater.inflate(
                 getLayoutId(currentPage), container, false)
+        createViewHolders(currentPage, rootView)
+
         rootView.findViewById<Button>(R.id.register_next_step)?.setOnClickListener {
             if (mCallback != null) mCallback!!.onNextButtonPressed(currentPage)
         }
@@ -52,7 +60,33 @@ class RegisterPageFragment : Fragment() {
         }
     }
 
+    private fun createViewHolders(position: Int, rootView: View) {
+        when (position) {
+            BIO_PAGE -> bioPageViewHolder = BioPageViewHolder(rootView)
+            PLACE_PAGE -> placePageViewHolder = PlacePageViewHolder(rootView)
+            CREDENTIALS_PAGE -> credentialsPageViewHolder = CredentialsPageViewHolder(rootView)
+            else ->credentialsPageViewHolder = CredentialsPageViewHolder(rootView)
+        }
+    }
+
     fun setCallback(callback: Callback) {
         this.mCallback = callback
+    }
+
+    class BioPageViewHolder(rootView: View) {
+        private val firstName: TextView = rootView.findViewById(R.id.register_bio_first_name)
+        private val SecondName: TextView = rootView.findViewById(R.id.register_bio_first_name)
+        private val birthday: TextView = rootView.findViewById(R.id.register_bio_first_name)
+    }
+
+    class PlacePageViewHolder(rootView: View) {
+        private val countryName: TextView = rootView.findViewById(R.id.register_country)
+        private val cityName: TextView = rootView.findViewById(R.id.register_city)
+    }
+
+    class CredentialsPageViewHolder(rootView: View){
+        private val login: TextView = rootView.findViewById(R.id.register_login)
+        private val password: TextView = rootView.findViewById(R.id.register_password)
+        private val repeatPassword: TextView = rootView.findViewById(R.id.register_repeat_password)
     }
 }
